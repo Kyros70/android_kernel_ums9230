4e4b55376c5eb1e6f7d1459ea59bd61c88a7c6bb
Kyros70 <zaedmasri000999@gmail.com>
2026-09-11 13:12:31 +0300

    Add files via upload

--- fs/read_write.c
+++ fs/read_write.c
@@ -602,7 +602,7 @@ ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 }
 
 #ifdef CONFIG_KSU_SUSFS
-extern bool ksu_vfs_read_hook __read_mostly;
+extern bool ksu_init_rc_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
                char __user **buf_ptr, size_t *count_ptr);
 #endif
@@ -610,7 +610,7 @@ extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 #ifdef CONFIG_KSU_SUSFS
-	if (unlikely(ksu_vfs_read_hook))
+	if (unlikely(ksu_init_rc_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
 	return ksys_read(fd, buf, count);
